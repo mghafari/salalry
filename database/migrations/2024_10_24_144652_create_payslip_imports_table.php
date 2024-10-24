06e924e4ef01398b34ee361079e16dc203311079
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-10-24 18:37:25 +0330

    add import model and migration

---
+++ database/migrations/2024_10_24_144652_create_payslip_imports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payslip_imports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('payslip_head_setting_id')->constrained('payslip_head_settings');
+            $table->integer('index');
+            $table->string('value');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payslip_imports');
+    }
+};
