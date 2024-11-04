d088f8ddb3594db2beb5581d31bb5e64f0f20e57
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-11-04 10:56:37 +0330

    add seeder for guarantee form status

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
+                GuaranteeFormStatusSeeder::class,
                 UserSeeder::class
             ]
         );
